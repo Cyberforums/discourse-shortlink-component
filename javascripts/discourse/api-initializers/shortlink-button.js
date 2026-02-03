0c8820acb4cee0d8fb48fb7aec53b7074d0f75cd
feibisi <support@feibisi.com>
2026-02-04 01:36:04 +0800

    fix: 使用正确的 outlet 名称 header-topic-info__after

--- javascripts/discourse/api-initializers/shortlink-button.js
+++ javascripts/discourse/api-initializers/shortlink-button.js
@@ -2,5 +2,5 @@ import { apiInitializer } from "discourse/lib/api";
 import ShortlinkDisplay from "../components/shortlink-display";
 
 export default apiInitializer("0.11.1", (api) => {
-  api.renderInOutlet("topic-title-after", ShortlinkDisplay);
+  api.renderInOutlet("header-topic-info__after", ShortlinkDisplay);
 });
